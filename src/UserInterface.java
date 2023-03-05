314d30ebb763c6bb7b1c70982a0e20776a7daab5
Lee Chapman <me@leejonchapman.com>
2023-03-05 16:14:52 +0000

    setting up

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,12 +1,23 @@
 import java.util.Scanner;
 
 public class UserInterface {
-Scanner userInput;
+    Scanner userInput;
 
     public UserInterface(Scanner userInput) {
         this.userInput = userInput;
     }
-    public void start(){
 
+    public void start() {
+        boolean runMenu = true;
+        System.out.print(
+                "Add - adds a bird \n" +
+                        "Observation - adds an observation \n" +
+                        "All - prints all birds \n" +
+                        "One - prints one bird \n" +
+                        "Quit - ends the program \n");
+    while (runMenu){
+        System.out.print("? ");
+        String uI = userInput.nextLine();
+    }
     }
 }
