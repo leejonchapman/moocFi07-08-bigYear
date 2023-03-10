6079d4daeaccd8f51ce11bd8888b474e05fc9f74
Lee Chapman <me@leejonchapman.com>
2023-03-10 19:24:43 +0000

    passes all tests

--- src/mainProgram.java
+++ src/mainProgram.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+// camel case as MOOC.FI errors without it
 public class mainProgram {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
